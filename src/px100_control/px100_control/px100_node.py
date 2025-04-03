b9ff694ac88aee5e22c849a8772e911d753a6692
Aly Tawfik <alyhatemtawfik@gmail.com>
2025-04-03 11:44:48 +0100

    Functional Pick & Drop Code

--- src/px100_control/px100_control/px100_node.py
+++ src/px100_control/px100_control/px100_node.py
@@ -22,8 +22,8 @@ class PX100Node(Node):
 
         self.place_object_server = self.create_service(
             srv_type=Trigger,
-            srv_name="/leo_rover/place_object",
-            callback=self.place_callback
+            srv_name="/leo_rover/drop_object",
+            callback=self.drop_callback
         )
 
         self.pose_subscriber = self.create_subscription(
@@ -33,10 +33,15 @@ class PX100Node(Node):
             qos_profile=1
         )
 
-        self.H_base_cam = np.array([[0, 0, 1,  0.062],
-                                    [1, 0, 0, -0.066],
-                                    [0, 1, 0,  0.006],
-                                    [0, 0, 0,      1]])
+        self.H_base_cam = np.array([[ 0, 0, 1,  0.062],
+                                    [-1, 0, 0, -0.066],  ### This is the value when camera is in position 1
+                                    [ 0, 1, 0,  0.006],
+                                    [ 0, 0, 0,      1]])
+        
+        # self.H_base_cam = np.array([[ 0, 0, 1,  0.066], ### This is the values when camera is offset (position 2)
+        #                             [-1, 0, 0, -0.013],  
+        #                             [ 0, 1, 0,   0.07],
+        #                             [ 0, 0, 0,      1]])
         self.H_cam_obj = np.zeros(4)
         self.H_base_obj = np.zeros(4)
         self.bot = InterbotixManipulatorXS(
@@ -55,19 +60,18 @@ class PX100Node(Node):
 
         self.H_base_obj = self.H_base_cam @ self.H_cam_obj
 
-        x = self.H_base_obj[0, 3]
+        x = self.H_base_obj[0, 3] + 0.0075
         y = self.H_base_obj[1, 3]
         z = self.H_base_obj[2, 3]
-        r = np.hypot(x, y) + 0.015
-        theta = np.arctan2(y, x)
-        x_traj = r - 0.2458
-        z_traj = z - 0.193
+        # r = np.hypot(x, y) + 0.015
+        # theta = np.arctan2(y, x)
+        # x_traj = r - 0.2458
+        # z_traj = z - 0.193
 
         self.bot.arm.go_to_home_pose()
         # self.bot.arm.set_single_joint_position(joint_name='waist', position=theta)
         self.bot.gripper.release()
-        self.bot.arm.set_ee_pose_components(x=r, z=z, pitch=0.0, blocking=True)
-        # self.bot.arm.set_ee_cartesian_trajectory(x=x_traj, z=z_traj)
+        self.bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.0, blocking=True)
         self.bot.gripper.grasp()
         self.bot.arm.go_to_home_pose(moving_time=1.5, blocking=True)
         self.bot.arm.go_to_sleep_pose()
@@ -80,14 +84,13 @@ class PX100Node(Node):
 
         return response
 
-    def place_callback(self,
+    def drop_callback(self,
                          request: Trigger.Request,
                          response: Trigger.Response
                          ) -> Trigger.Response:
 
         self.bot.arm.go_to_home_pose()
-        self.bot.arm.set_single_joint_position(joint_name='waist', position=pi/2)
-        self.bot.arm.set_ee_pose_components(x=0.2, z=0.03, blocking=True)
+        self.bot.arm.set_joint_positions([0.0, pi/4, -pi/4, 0.0])
         self.bot.gripper.release()
         self.bot.arm.go_to_home_pose()
         self.bot.arm.go_to_sleep_pose()
